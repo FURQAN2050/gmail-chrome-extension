91137f1ef7a9138b18de08da01367d2561711489
furqan2050 <mfurqan@folio3.com>
2022-04-09 14:04:53 +0500

    changes till now.

--- src/app/components/emails/emails.component.ts
+++ src/app/components/emails/emails.component.ts
@@ -10,16 +10,10 @@ import { AddEmailsModalComponent } from './add-emails-modal/add-emails-modal.com
 export class EmailsComponent implements OnInit {
   displayedColumns: string[] = ['GroupName', 'Emails'];
   ListData = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+    {position: 1, name: 'John@gmail.com', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Doe@gmail.com', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Smith@gmail.com', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'casan@gmail.com', weight: 9.0122, symbol: 'Be'},
   ];
   constructor(public dialog: MatDialog) { }
   addEmails(){
