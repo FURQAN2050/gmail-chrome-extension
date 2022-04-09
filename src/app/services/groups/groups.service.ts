29571bf4c99388148e03f1c1542d9ce6209ec44f
furqan2050 <mfurqan@folio3.com>
2022-04-09 20:38:42 +0500

    group service.

---
+++ src/app/services/groups/groups.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {GroupApi,GroupemailApi} from '../../shared/sdk';
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupsService {
+
+  constructor(private GroupApi:GroupApi,private GroupemailApi: GroupemailApi ) { }
+
+  lookupGroups(filters:any){
+    this.GroupApi.find(filters).subscribe((res: any)=>{
+      console.log(res);
+    })
+  }
+
+  async upsertGroupEmailAPI(payload:any){
+    if(!(payload instanceof Array)){
+     payload=[payload]; 
+    }
+    for(let i=0;i<payload.length;i++){
+      let groupEmail=payload[i];
+      await this.GroupemailApi.create(groupEmail).toPromise();
+    }
+  }
+
+  async upsertGroup(payload:any){
+    if(payload.id){
+      await this.GroupApi.create(payload).toPromise();
+    }else{
+      await this.GroupApi.updateAttributes(payload.id,payload).toPromise();
+    }
+  }
+
+
+  async deleteGroupEmail(id:any){
+    await this.GroupemailApi.deleteById(id).toPromise(); 
+  }
+
+}
