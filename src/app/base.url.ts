2513effcf2d0314150e783144521e872f4d8be5d
furqan2050 <mfurqan@folio3.com>
2022-04-18 03:38:30 +0500

    change base url

--- src/app/base.url.ts
+++ src/app/base.url.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:3000'; 
-// export const BASE_URL = '<base url production>'; 
-export const API_VERSION = 'api'; 
+// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'http://34.200.245.210:3000'; //prod url
+export const API_VERSION = 'api';
