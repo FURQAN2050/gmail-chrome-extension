3bb0c3850f93f9b6097a2655dd8fa3076379d5fa
furqan2050 <mfurqan@folio3.com>
2022-04-29 07:21:41 +0500

    change base_url.

--- src/app/base.url.ts
+++ src/app/base.url.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'http://localhost:3000';
-export const BASE_URL = 'http://34.200.245.210:3000'; //prod url
+export const BASE_URL = 'https://chrome.myprojectstaging.com:3000'; //prod url
 export const API_VERSION = 'api';
