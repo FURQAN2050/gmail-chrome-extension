b39a663cd425eca2a86fc563b217cf74902c7bb8
furqan2050 <mfurqan@folio3.com>
2022-07-09 00:38:38 +0500

    extension = add the support of templates.

--- extensioncode2/hello-world/src/content.js
+++ extensioncode2/hello-world/src/content.js
@@ -91,6 +91,10 @@ class Toggle extends React.Component {
     this.getGroupData = this.getGroupData.bind(this);
     this.signout = this.signout.bind(this);
     this.closeWidget = this.closeWidget.bind(this);
+    this.showGroupTemplates = this.showGroupTemplates.bind(this);
+    this.setTemplatesOnEmailBody = this.setTemplatesOnEmailBody.bind(this);
+
+
     //this.checkloggedIn();
     if (this.state.userId) {
       this.setState({ isloggedIn: true })
@@ -115,153 +119,12 @@ class Toggle extends React.Component {
       emails.push(email);
     }
     COMPOSEVIEW.setToRecipients(emails);
-    COMPOSEVIEW.setBodyHTML(`
-    <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
-    <head>
-    <!--[if gte mso 9]>
-    <xml>
-      <o:OfficeDocumentSettings>
-        <o:AllowPNG/>
-        <o:PixelsPerInch>96</o:PixelsPerInch>
-      </o:OfficeDocumentSettings>
-    </xml>
-    <![endif]-->
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="x-apple-disable-message-reformatting">
-      <!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]-->
-      <title></title>
-      
-        <style type="text/css">
-          @media only screen and (min-width: 520px) {
-      .u-row {
-        width: 500px !important;
-      }
-      .u-row .u-col {
-        vertical-align: top;
-      }
-    
-      .u-row .u-col-100 {
-        width: 500px !important;
-      }
-    
-    }
-    
-    @media (max-width: 520px) {
-      .u-row-container {
-        max-width: 100% !important;
-        padding-left: 0px !important;
-        padding-right: 0px !important;
-      }
-      .u-row .u-col {
-        min-width: 320px !important;
-        max-width: 100% !important;
-        display: block !important;
-      }
-      .u-row {
-        width: calc(100% - 40px) !important;
-      }
-      .u-col {
-        width: 100% !important;
-      }
-      .u-col > div {
-        margin: 0 auto;
-      }
-    }
-    body {
-      margin: 0;
-      padding: 0;
-    }
-    
-    table,
-    tr,
-    td {
-      vertical-align: top;
-      border-collapse: collapse;
-    }
-    
-    .ie-container table,
-    .mso-container table {
-      table-layout: fixed;
-    }
-    
-    * {
-      line-height: inherit;
-    }
-    
-    a[x-apple-data-detectors='true'] {
-      color: inherit !important;
-      text-decoration: none !important;
-    }
-    
-    table, td { color: #000000; } </style>
-      
-      
-    
-    </head>
-    
-    <body class="clean-body u_body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #e7e7e7;color: #000000">
-      <!--[if IE]><div class="ie-container"><![endif]-->
-      <!--[if mso]><div class="mso-container"><![endif]-->
-      <table style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #e7e7e7;width:100%" cellpadding="0" cellspacing="0">
-      <tbody>
-      <tr style="vertical-align: top">
-        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
-        <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #e7e7e7;"><![endif]-->
-        
-    
-    <div class="u-row-container" style="padding: 0px;background-color: transparent">
-      <div class="u-row" style="Margin: 0 auto;min-width: 320px;max-width: 500px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
-        <div style="border-collapse: collapse;display: table;width: 100%;background-color: transparent;">
-          <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:500px;"><tr style="background-color: transparent;"><![endif]-->
-          
-    <!--[if (mso)|(IE)]><td align="center" width="500" style="width: 500px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;" valign="top"><![endif]-->
-    <div class="u-col u-col-100" style="max-width: 320px;min-width: 500px;display: table-cell;vertical-align: top;">
-      <div style="width: 100% !important;">
-      <!--[if (!mso)&(!IE)]><!--><div style="padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;"><!--<![endif]-->
-      
-    <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
-      <tbody>
-        <tr>
-          <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
-            
-      <div style="line-height: 140%; text-align: left; word-wrap: break-word;">
-        <ol>
-    <li style="font-size: 14px; line-height: 19.6px;">Hello this is Furqan</li>
-    </ol>
-      </div>
-    
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    
-      <!--[if (!mso)&(!IE)]><!--></div><!--<![endif]-->
-      </div>
-    </div>
-    <!--[if (mso)|(IE)]></td><![endif]-->
-          <!--[if (mso)|(IE)]></tr></table></td></tr></table><![endif]-->
-        </div>
-      </div>
-    </div>
-    
-    
-        <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
-        </td>
-      </tr>
-      </tbody>
-      </table>
-      <!--[if mso]></div><![endif]-->
-      <!--[if IE]></div><![endif]-->
-    </body>
-    
-    </html>`)
+    // COMPOSEVIEW.setBodyHTML();
   }
 
   getGroupData(userId) {
     console.log(userId);
-    let response = fetch(`https://chrome.myprojectstaging.com:3000/api/groups?filter={"where": {"enduserId":${userId}},"include":["emails"]}`)
+    let response = fetch(`https://chrome.myprojectstaging.com:3000/api/groups?filter={"where": {"enduserId":${userId}},"include":["emails","templates"]}`)
       .then(response => response.json())
       .then(res => {
         console.log(res);
@@ -328,6 +191,36 @@ class Toggle extends React.Component {
     }
   }
 
+  setTemplatesOnEmailBody(template) {
+    console.log(JSON.parse(template.html));
+    COMPOSEVIEW.setBodyHTML(JSON.parse(template.html));
+  }
+
+  showGroupTemplates(templates) {
+    const style = {
+      'font-size': '16px;',
+      "margin": "10px",
+      "background-color": "#04AA6D",
+      "border-radius": "16px",
+      "height": "30px",
+      "background": "white"
+    }
+    console.log(templates)
+    let finalTempaltes = [];
+    for (let i = 0; i < templates.length; i++) {
+      finalTempaltes.push(
+        <button style={style} onClick={() => this.setTemplatesOnEmailBody(templates[i])}>
+          {templates[i].name}
+        </button>
+      )
+    }
+    if (finalTempaltes.length) {
+      return (<div>{finalTempaltes}</div>)
+    } else {
+      return (<div></div>)
+    }
+  }
+
 
   render() {
     let groupsDataDiv = [];
@@ -348,11 +241,17 @@ class Toggle extends React.Component {
       console.log(this.state.data[i]);
       let group = this.state.data[i].name;
       console.log(group);
-      groupsDataDiv.push(<div>
-        <button style={style} onClick={() => this.handleClick(this.state.data[i])}>
-          {group}
-        </button>
-      </div>);
+      groupsDataDiv.push(
+        <div>
+          <button style={style} onClick={() => this.handleClick(this.state.data[i])}>
+            {group}
+          </button>
+          <div>
+            {this.showGroupTemplates(this.state.data[i].templates)}
+          </div>
+
+        </div>
+      );
     }
     if (this.state.isloggedIn) {
       return (
